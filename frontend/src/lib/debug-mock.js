f2d5fcdb4f3c85c4b25b941843852098059b901e
Leoo3s <souz4leonardo@gmail.com>
2026-01-08 20:39:56 -0300

    🚀 FINAL FIX: Escudo Global e Limpeza de Dependências

--- frontend/src/lib/debug-mock.js
+++
@@ -1,8 +0,0 @@
-
-// Versão Mockada do Debug para Browser para evitar erro no Vite
-export default function debug(namespace) {
-    // Retorna uma função vazia (noop) em produção, ou console.log se quiser debugar
-    return (...args) => {
-        // console.log(`[${namespace}]`, ...args); 
-    };
-}
