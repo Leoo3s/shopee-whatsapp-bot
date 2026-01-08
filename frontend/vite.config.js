e307042d2ef48d08c776c3987253fe64e880d0df
Leoo3s <souz4leonardo@gmail.com>
2026-01-08 20:34:42 -0300

    🔧 Fix: Resolvido conflito de ambiente do Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    define: {
-        global: 'window',
-    },
     resolve: {
         alias: {
             debug: path.resolve(__dirname, 'src/lib/debug-mock.js')
